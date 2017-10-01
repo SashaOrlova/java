5da72750d594bd5147a167e297d1aff234820afb
mac <mac@MacBook-Pro-mac.local>
2017-10-01 16:16:09 +0300

    Change Object to int. Add test with even matrix length

--- Spiral/src/main/java/ru/homework/java/Spiral.java
+++ Spiral/src/main/java/ru/homework/java/Spiral.java
@@ -1,23 +1,27 @@
+package ru.homework.java;
+
 import java.util.Arrays;
 
 public class Spiral {
     /**
-     * print two-dimensional array in spiral circumvention
+     * print two-dimensional array in spiral circumvention.
+     * Throw IllegalArgumentException if matrix has even length.
      *
-     * @param matrix - two-dimensional array that will be printed. Please do not use primitive types.
+     * @param matrix - two-dimensional array of int that will be printed.
      * @return string that contain all elements of matrix in their spiral circumvention
      */
-    public static String spiral(final Object[][] matrix) {
+    public static String spiral(final int[][] matrix) throws IllegalArgumentException {
+        if (matrix.length % 2 == 0) throw new IllegalArgumentException("Even matrix size");
         StringBuilder answer = new StringBuilder();
         int start_pos = matrix.length / 2;
-        answer.append(matrix[start_pos][start_pos].toString());
+        answer.append(matrix[start_pos][start_pos]);
         int cnt = 1;
         while (cnt < matrix.length / 2 + 1) {
             for (int i = 0; i < cnt * 2; i++) {
-                answer.append(matrix[start_pos - cnt + i + 1][start_pos + cnt].toString());
+                answer.append(matrix[start_pos - cnt + i + 1][start_pos + cnt]);
             }
             for (int i = 0; i < cnt * 2; i++) {
-                answer.append(matrix[start_pos + cnt][start_pos + cnt - i - 1].toString());
+                answer.append(matrix[start_pos + cnt][start_pos + cnt - i - 1]);
             }
             for (int i = 0; i < cnt * 2; i++) {
                 answer.append(matrix[start_pos + cnt - i - 1][start_pos - cnt]);
@@ -35,20 +39,22 @@ public class Spiral {
      *
      * @param matrix - two-dimensional array that will be sorted
      */
-    public static void sortMatrix(Comparable[][] matrix) {
-        for (int i = 0 ; i < matrix.length; i++)
-            for (int j = i + 1 ; j < matrix.length; j++){
-                Comparable tmp = matrix[i][j];
+    public static void sortMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix.length; j++) {
+                int tmp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
             }
-        Arrays.sort(matrix, (o1, o2) -> o2[0].compareTo(o1[0]));
-        for (int i = 0 ; i < matrix.length; i++)
-            for (int j = i+1 ; j < matrix.length; j++){
-                Comparable tmp = matrix[i][j];
+        }
+        Arrays.sort(matrix, (int[] o1, int[] o2) -> o2[0] - o1[0]);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix.length; j++) {
+                int tmp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = tmp;
             }
+        }
     }
 
     static public void main(String[] st) {
--- Spiral/src/test/java/SpiralTest.java
+++ Spiral/src/test/java/SpiralTest.java
@@ -1,58 +1,84 @@
+import ru.homework.java.Spiral;
+
 import static org.junit.Assert.assertEquals;
 
 class SpiralTest {
     /**
-     * test how Spiral works with different arguments.
-     * Include test for Object = Integer.
+     * test how Spiral works with different size of matrix. Include tests for methods spiral() and sort.
      */
     @org.junit.jupiter.api.Test
     void spiralTestSimple() {
-        Integer[][] m = new Integer[1][1];
+        int[][] m = new int[1][1];
         int c = 1;
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 1; i++) {
             for (int j = 0; j < 1; j++) {
                 m[i][j] = c++;
             }
+        }
         assertEquals(Spiral.spiral(m), "1");
     }
 
     @org.junit.jupiter.api.Test
     void spiralTestThree() {
-        Integer[][] m = new Integer[3][3];
+        int[][] m = new int[3][3];
         int c = 1;
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 m[i][j] = c++;
             }
+        }
         assertEquals(Spiral.spiral(m), "569874123");
     }
 
     @org.junit.jupiter.api.Test
     void spiralTestFive() {
-        Integer[][] m = new Integer[5][5];
+        int[][] m = new int[5][5];
         int c = 1;
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 m[i][j] = c++;
             }
+        }
         assertEquals(Spiral.spiral(m), "13141918171278910152025242322211611612345");
     }
 
     @org.junit.jupiter.api.Test
+    void spiralTestEvenSize() {
+        int[][] m = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                m[i][j] = 0;
+            }
+        }
+        try {
+            Spiral.spiral(m);
+        } catch (IllegalArgumentException thrown) {
+            assertEquals("Even matrix size", thrown.getMessage());
+        }
+    }
+
+    /**
+     * sort matrix size [1x1]
+     */
+    @org.junit.jupiter.api.Test
     void spiralTestSimpleSort() {
-        Integer[][] m = new Integer[1][1];
+        int[][] m = new int[1][1];
         int c = 1;
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 1; i++) {
             for (int j = 0; j < 1; j++) {
                 m[i][j] = c++;
             }
+        }
         Spiral.sortMatrix(m);
-        assertEquals(m[0][0].toString(), "1");
+        assertEquals(m[0][0], 1);
     }
 
+    /**
+     * sort matrix size [5x5] full numbers from 1 to 25
+     */
     @org.junit.jupiter.api.Test
     void spiralTestSort() {
-        Integer[][] m = new Integer[5][5];
+        int[][] m = new int[5][5];
         int c = 1;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -60,15 +86,15 @@ class SpiralTest {
             }
         }
         Spiral.sortMatrix(m);
-        Integer[][] n = new Integer[5][5];
+        int[][] n = new int[5][5];
         for (int i = 0; i < 5; i++) {
             c = (i + 1) * 5;
             for (int j = 0; j < 5; j++) {
                 n[i][j] = c - j;
             }
         }
-        for (int i = 0 ; i < 5; i++) {
-            for (int j = 0; j < 5; j++){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 assertEquals(m[i][j], n[i][j]);
             }
         }
