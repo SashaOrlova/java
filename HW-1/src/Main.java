948fcc373d28acf9a8a9119fad17750ca661e1cb
mac <mac@MacBook-Pro-mac.local>
2017-10-02 23:42:49 +0300

    Add comments for List

--- HW-1/src/Main.java
+++ HW-1/src/Main.java
@@ -1,7 +1,6 @@
 import ru.java.homework.HashMap;
-import ru.java.homework.HashMapTest;
+//import ru.java.homework.HashMapTest;
 
-import static java.lang.System.exit;
 
 public class Main {
     static boolean isSuccess(boolean condition, String message) {
@@ -14,15 +13,20 @@ public class Main {
 
     public static void main(String[] args) {
         HashMap test = new HashMap();
+        boolean isTestsPass = true;
         test.put("aa", "bb");
         test.put("cc","dd");
-        isSuccess(test.get("aa").equals("bb"), "Wrong get");
-        isSuccess(test.get("cc").equals("dd"), "Wrong get");
-        isSuccess(test.size() == 2, "Wrong size");
-        isSuccess(test.contains("cc"), "Wrong contains");
-        isSuccess(test.put("aa","ss").equals("bb"), "Wrong put");
-        isSuccess(test.remove("cc").equals("dd"), "Wrong remove");
-        isSuccess(test.get("aa").equals("ss"), "Wrong get");
-        isSuccess(test.get("cc") == null, "Wrong get");
+        isTestsPass &= isSuccess(test.get("aa").equals("bb"), "Wrong get");
+        isTestsPass &= isSuccess(test.get("cc").equals("dd"), "Wrong get");
+        isTestsPass &= isSuccess(test.size() == 2, "Wrong size");
+        isTestsPass &= isSuccess(test.contains("cc"), "Wrong contains");
+        isTestsPass &= isSuccess(test.put("aa","ss").equals("bb"), "Wrong put");
+        isTestsPass &= isSuccess(test.remove("cc").equals("dd"), "Wrong remove");
+        isTestsPass &= isSuccess(test.get("aa").equals("ss"), "Wrong get");
+        isTestsPass &= isSuccess(test.get("cc") == null, "Wrong get");
+        test.clear();
+        isTestsPass &= isSuccess(test.size() == 0, "Wrong get");
+        if (isTestsPass)
+            System.out.println("All tests pass!");
     }
 }
--- HW-1/src/ru/java/homework/HashMap.java
+++ HW-1/src/ru/java/homework/HashMap.java
@@ -3,7 +3,7 @@ package ru.java.homework;
 import static java.lang.Math.abs;
 
 /**
- * Hashmap is a data structure which implements an associative array abstract data type,
+ * is a data structure which implements an associative array abstract data type,
  * a structure that can map keys to values.
  */
 public class HashMap {
@@ -15,32 +15,32 @@ public class HashMap {
     private List[] lists;
 
     /**
-     * Constructs an empty HashMap with the default initial capacity (73) and the default load factor (0.75).
+     * constructs an empty HashMap with the default initial capacity (73) and the default load factor (0.75).
      */
     public HashMap() {
         lists = new List[capacity];
-        for (int i = 0 ; i < capacity; i++) {
+        for (int i = 0; i < capacity; i++) {
             lists[i] = new List();
         }
     }
 
-    private int getHashCode(String St){
+    private int getHashCode(String St) {
         return abs(St.hashCode() % (capacity));
     }
 
     private boolean isFull() {
-        return size >= 0.75*capacity;
+        return size >= 0.75 * capacity;
     }
 
     private void rebuild() {
-        List[] newLists = new List[capacity*2];
-        for (int i = 0 ; i < capacity*2; i++) {
+        List[] newLists = new List[capacity * 2];
+        for (int i = 0; i < capacity * 2; i++) {
             newLists[i] = new List();
         }
         for (int i = 0; i < capacity; i++) {
             List.Node now = lists[i].head;
             while (now != null) {
-                newLists[abs(now.keyValue.hashCode() % (capacity*2))].insert(now.stringValue, now.keyValue);
+                newLists[abs(now.keyValue.hashCode() % (capacity * 2))].insert(now.stringValue, now.keyValue);
                 now = now.nextNode;
             }
         }
@@ -54,8 +54,10 @@ public class HashMap {
     public int size() {
         return size;
     }
+
     /**
-     * Returns true if key was found, or false if this map contains no mapping for the key.
+     * returns true if key was found, or false if this map contains no mapping for the key.
+     *
      * @param key the key whose associated value is to be found
      * @return true if the key was found else false
      */
@@ -64,8 +66,9 @@ public class HashMap {
     }
 
     /**
-     * Returns the value to which the specified key is mapped,
-     or null if this map contains no mapping for the key.
+     * returns the value to which the specified key is mapped,
+     * or null if this map contains no mapping for the key.
+     *
      * @param key the key whose associated value is to be returned
      * @return the value mapping with key
      */
@@ -74,9 +77,10 @@ public class HashMap {
     }
 
     /**
-     * Associates the value with the key in this map.
-     If the map previously contained a mapping for the key, the old value will be replaced.
-     * @param key  key with which the value to be associated
+     * associates the value with the key in this map.
+     * If the map previously contained a mapping for the key, the old value will be replaced.
+     *
+     * @param key   key with which the value to be associated
      * @param value value to be associated with key
      * @return old value associated with key
      */
@@ -91,7 +95,8 @@ public class HashMap {
     }
 
     /**
-     * Removes the mapping for the key from this map
+     * removes the mapping for the key from this map
+     *
      * @param key key with which the value to be associated
      * @return value associated with key
      */
@@ -103,36 +108,28 @@ public class HashMap {
     }
 
     /**
-     * Removes all keys and values from the map
+     * removes all keys and values from the map
      */
     public void clear() {
         size = 0;
         capacity = initial_capacity;
         lists = new List[capacity];
-        for (int i = 0 ; i < capacity; i++) {
+        for (int i = 0; i < capacity; i++) {
             lists[i] = new List();
         }
     }
+
+    /**
+     * additional class for keep collisions in hashmap. Store keys with value.
+     */
     private class List {
-        private class Node {
-            private Node nextNode;
-            private Node prevNode;
-
-            private String stringValue;
-            private String keyValue;
-
-            private Node(String stringValue, String key) {
-                this.stringValue = stringValue;
-                keyValue = key;
-            }
-        }
         private Node head = null;
         private int size = 0;
+
         private void insert(String stringValue, String key) {
             if (size == 0) {
                 head = new Node(stringValue, key);
-            }
-            else {
+            } else {
                 Node now = head;
                 while (now.nextNode != null) {
                     now = now.nextNode;
@@ -142,6 +139,13 @@ public class HashMap {
             }
             size++;
         }
+
+        /**
+         * check key in list
+         *
+         * @param key string that wont be found
+         * @return value stored with key or null if key not in list
+         */
         private String search(String key) {
             Node now = head;
             while (now != null && !now.keyValue.equals(key)) {
@@ -152,10 +156,23 @@ public class HashMap {
             }
             return null;
         }
+
+        /**
+         * check existence of key in list
+         *
+         * @param key string
+         * @return true if key in list, no otherwise
+         */
         private boolean find(String key) {
-            String try_search = search(key);
-            return  try_search != null;
+            String trySearch = search(key);
+            return trySearch != null;
         }
+
+        /**
+         * delete key and value from list
+         *
+         * @param key value with key will be deleted
+         */
         private void delete(String key) {
             Node now = head;
             while (now != null && !now.keyValue.equals(key)) {
@@ -170,13 +187,28 @@ public class HashMap {
                         now.prevNode.nextNode = now.nextNode;
                     }
                     now.nextNode.prevNode = now.prevNode;
-                }
-                else{
+                } else {
                     if (now.prevNode != null) {
                         now.prevNode.nextNode = null;
                     }
                 }
                 size--;
+            }
+        }
+
+        /**
+         * class describe one element of list
+         */
+        private class Node {
+            private Node nextNode;
+            private Node prevNode;
+
+            private String stringValue;
+            private String keyValue;
+
+            private Node(String stringValue, String key) {
+                this.stringValue = stringValue;
+                keyValue = key;
             }
         }
     }
