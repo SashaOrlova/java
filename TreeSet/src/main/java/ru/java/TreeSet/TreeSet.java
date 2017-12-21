bdba7041e4f7daec127813ea08f197b207d563e3
SashaOrlova <sasha.orlova.1998n@mail.ru>
2017-12-21 12:01:08 +0300

    add javadoc, add anatations, initialize comparator, separate Tree and Set, add more tests

--- TreeSet/src/main/java/ru/java/TreeSet/TreeSet.java
+++ TreeSet/src/main/java/ru/java/TreeSet/TreeSet.java
@@ -1,276 +1,329 @@
 package ru.java.TreeSet;
-
-
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.java.MyTreeSet.MyTreeSet;
 
 import java.util.AbstractSet;
 import java.util.Comparator;
 import java.util.Iterator;
 
+/**TreeSet provides an implementation of the Set interface that uses a tree for storage.
+ * Objects are stored in a sorted order.
+ * @param <E> - type of stored Objects
+ */
 public class TreeSet<E> extends AbstractSet<E> implements MyTreeSet<E> {
-    protected int size = 0;
-    protected Vertex root;
-    protected Comparator<? super E> cmp;
-    protected Vertex first;
-    protected Vertex last;
+    protected Tree tree = new Tree();
+    private Comparator<? super E> cmp;
 
-    TreeSet(Comparator<? super E> cmp) {
+    /**
+     * Create empty TreeSet sorted according to the specified comparator.
+     * @param cmp - the comparator that will be used to order this set. If null,
+     *           the natural ordering of the elements will be used.
+     */
+    public TreeSet(@NotNull Comparator<? super E> cmp) {
         this.cmp = cmp;
     }
 
-    TreeSet() {}
+    /**
+     * Create empty TreeSet sorted according to the natural ordering of its elements.
+     */
+    @SuppressWarnings("unchecked")
+    public TreeSet() {
+        cmp = (x, y) -> ((Comparable<? super E>)x).compareTo(y);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<E> descendingIterator() {
+        return new Iterator<E>() {
+            Tree.Vertex current = tree.last;
+
+            @Override
+            public boolean hasNext() {
+                return current != tree.first;
+            }
+
+            @Override
+            public E next() {
+                Tree.Vertex prev = current;
+                current = current.prev;
+                return prev.value;
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public MyTreeSet<E> descendingSet() {
+        return new DescendingSet(this);
+    }
+
+    @Nullable
+    @Override
+    public E first() {
+        return tree.first == null ? null : tree.first.value;
+    }
+
+    @Nullable
+    @Override
+    public E last() {
+        return tree.last == null ? null : tree.last.value;
+    }
+
+
+    @Nullable
+    @Override
+    public E lower(E e) {
+        Tree.Vertex MaybeAns = tree.lowerBound(tree.root, e, tree.root);
+        return MaybeAns == null ? null : (MaybeAns.prev == null ? null : MaybeAns.prev.value);
+    }
+
+    @Nullable
+    @Override
+    public E floor(E e) {
+        Tree.Vertex MaybeAns = tree.upperBound(tree.root, e, tree.root);
+        return MaybeAns == null ? null : (MaybeAns.prev == null ? null : MaybeAns.prev.value);
+    }
+
+    @Nullable
+    @Override
+    public E ceiling(E e) {
+        Tree.Vertex MaybeAns = tree.lowerBound(tree.root, e, tree.root);
+        return MaybeAns == null ? null : MaybeAns.value;
+    }
+
+    @Nullable
+    @Override
+    public E higher(E e) {
+        Tree.Vertex MaybeAns = tree.upperBound(e);
+        return MaybeAns == null ? null : MaybeAns.value;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            Tree.Vertex current = tree.first;
+
+            @Override
+            public boolean hasNext() {
+                return current != tree.last;
+            }
+
+            @Override
+            public E next() {
+                Tree.Vertex prev = current;
+                current = current.next;
+                return prev.value;
+            }
+        };
+    }
 
     @Override
     public int size() {
-        return size;
-    }
-
-    private boolean find (Object  o, Vertex v) {
-        return  v != null && ( o.equals(v.value) || find(o, v.left) || find(o, v.right));
+        return tree.size;
     }
 
     @Override
-    public boolean contains(Object o) {
-        return find(o, root);
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new SetIterator();
-    }
-
-    @SuppressWarnings("unchecked")
-    private int comp(E a, E b) {
-        if (a == null || b == null)
-            return 0;
-        if (cmp == null) {
-            Comparable<? super E> c = (Comparable<? super E>)a;
-            return c.compareTo(b);
-        }
-        return cmp.compare(a, b);
+    public void clear() {
+        tree = new Tree();
     }
 
     @Override
     public boolean add(E e) {
-        size++;
-        if (root != null) {
-            Vertex position = root;
-            Vertex preposition = root;
-            while (position != null) {
-                preposition = position;
-                if (comp(position.value, e) == 0)
-                    return false;
-                if (comp(position.value, e) < 0) {
-                    position = position.left;
-                } else {
-                    position = position.right;
-                }
+        return tree.add(e);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean remove(Object e) {
+        return tree.remove((E) e);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean contains(Object o) {
+        return tree.find((E) o);
+    }
+
+    private class Tree {
+        private int size = 0;
+        private Vertex root;
+        private Vertex first;
+        private Vertex last;
+
+        private boolean add(E elem) {
+            if (find(elem)) return false;
+            if (size == 0) {
+                root = new Vertex(elem);
+                last = first = root;
+                size++;
+                return true;
             }
-            if (comp(preposition.value, e) == 0)
-                return false;
-            if (comp(preposition.value, e) < 0) {
-                Vertex node = new Vertex(e);
-                preposition.left = node;
-                node.next = preposition;
-                preposition.prev = node;
-                node.next = preposition.next;
-                preposition.next = node;
-                node.parent = preposition;
+            size++;
+            addInTree(root, elem);
+            Vertex newVertex = find(root, elem);
+            Vertex newNext = next(newVertex);
+            if (newNext != null) {
+                newVertex.next = newNext;
+                newVertex.prev = newNext.prev;
+                if (newVertex.prev != null)
+                    newVertex.prev.next = newVertex;
+                newNext.prev = newVertex;
+                if (newNext == first)
+                    first = newVertex;
             } else {
-                Vertex node = new Vertex(e);
-                preposition.right = node;
-                node.prev = preposition;
-                preposition.prev.next = node;
-                node.prev = preposition.prev;
-                preposition.prev = node;
-                node.parent = preposition;
+                last.next = newVertex;
+                newVertex.prev = last;
+                last = newVertex;
+            }
+            return true;
+        }
+
+        private Vertex addInTree(Vertex v, E elem) {
+            if (v == null)
+                return new Vertex(elem);
+            if (cmp.compare(elem, v.value) < 0) {
+                v.left = addInTree(v.left, elem);
+                v.left.parent = v;
+                return v;
+            }
+            v.right = addInTree(v.right, elem);
+            v.right.parent = v;
+            return v;
+        }
+
+        private Vertex next(Vertex v) {
+            if (v.right != null) {
+                while (v.left != null)
+                    v = v.left;
+                return v;
+            } else {
+                while (v.parent != null && v.parent.right == v)
+                    v = v.parent;
+                return v.parent;
             }
         }
-        else {
-            root = new Vertex(e);
-            first = root;
-            last = root;
-            first.next = last;
-            last.prev = first;
+
+        private void delFromList(Vertex vertex) {
+            if (vertex.next != null)
+                vertex.next.prev = vertex.prev;
+            if (vertex.prev != null)
+                vertex.prev.next = vertex.next;
+            if (vertex == first)
+                first = vertex.next;
+            if (vertex == last)
+                last = vertex.prev;
         }
-        if (last != null && last.next != null)
-            last = last.next;
-        if (first != null && first.prev != null)
-            first = first.prev;
-        return true;
-    }
 
-    private Vertex findEqVertex(Object o, Vertex v) {
-        if (v == null)
-            return null;
-        if (v.left != null && v.left.value.equals(o))
-            return v.left;
-        if (v.right != null && v.right.value.equals(o))
-            return v.right;
-        return v.value.equals(o) ? v : null;
-    }
+        private void delWithoutLeft(Vertex vertex) {
+            if (vertex.parent != null) {
+                if (vertex.parent.left != null && vertex.parent.left == vertex)
+                    vertex.parent.left = vertex.right;
+                else
+                    vertex.parent.right = vertex.right;
+            } else
+                root = vertex.right;
+            if (vertex.right != null)
+                vertex.right.parent = vertex.parent;
+        }
 
-    private void deleteFromList(Vertex v) {
-        v.prev.next = v.next;
-        v.next.prev = v.prev;
-        if (last.equals(v))
-            last = last.prev;
-        if (first.equals(v))
-            first = first.next;
-    }
+        private void delWithoutRight(Vertex vertex) {
+            if (vertex.parent != null) {
+                if (vertex.parent.left != null && vertex.parent.left == vertex)
+                    vertex.parent.left = vertex.left;
+                else
+                    vertex.parent.right = vertex.left;
+            } else
+                root = vertex.left;
+            vertex.left.parent = vertex.parent;
+        }
 
-    @Override
-    public boolean remove(Object o) {
-        size--;
-        Vertex v = findEqVertex(o, root);
-        if (v == null)
-            return false;
-        if (v.left == null && v.right == null) {
-            if (v.parent.right.value.equals(o))
-                v.parent.right = null;
+        private void delWithTwoChildren(Vertex vertex) {
+            Vertex nextVertex = vertex.next;
+            if (nextVertex.parent != null &&
+                    nextVertex.parent.right != null && nextVertex.parent.right == nextVertex)
+                nextVertex.parent.right = null;
+            else if (nextVertex.parent != null) {
+                nextVertex.parent.left = null;
+            }
+            if (vertex.parent != null) {
+                if (vertex.parent.right == vertex)
+                    vertex.parent.right = nextVertex;
+                else
+                    vertex.parent.left = nextVertex;
+            } else
+                root = nextVertex;
+            nextVertex.right = vertex.right;
+            nextVertex.left = vertex.left;
+        }
+
+        private Vertex upperBound(E elem) {
+            Vertex v = upperBound(root, elem, root);
+            return (cmp.compare(v.value, elem) < 0) ?
+                    null : v;
+        }
+        private Vertex upperBound(Vertex v, E elem, Vertex ans) {
+            if (v == null)
+                return ans;
+            if (cmp.compare(elem, v.value) < 0)
+                return upperBound(v.left, elem, cmp.compare(ans.value, v.value) < 0 ? ans : v);
             else
-                v.parent.left = null;
-            deleteFromList(v);
+                return upperBound(v.right, elem, ans);
+        }
+
+        private Vertex lowerBound(Vertex v, E elem, Vertex ans) {
+            Vertex maybeAns = find(root, elem);
+            return maybeAns == null ? upperBound(v, elem, ans) : maybeAns;
+        }
+
+        private boolean remove(E elem) {
+            Vertex vertex = find(root, elem);
+            if (vertex == null) return false;
+            size--;
+            if (vertex.left == null) {
+                delWithoutLeft(vertex);
+            } else if (vertex.right == null) {
+                delWithoutRight(vertex);
+            } else {
+                delWithTwoChildren(vertex);
+            }
+            delFromList(vertex);
             return true;
         }
-        if (v.right == null) {
-            if (v.parent.left != null && v.parent.left.value.equals(v)) {
-                v.parent.left = v.left;
-                v.left.parent = v.parent;
+
+        private boolean find(E elem) {
+            return find(root, elem) != null;
+        }
+
+        private Vertex find(Vertex v, E elem) {
+            if (v == null)
+                return null;
+            if (elem.equals(v.value))
+                return v;
+            if (cmp.compare(elem, v.value) < 0)
+                return find(v.left, elem);
+            else
+                return find(v.right, elem);
+        }
+
+        private class Vertex {
+            Vertex right;
+            Vertex left;
+            Vertex parent;
+            Vertex next;
+            Vertex prev;
+            private E value;
+
+            Vertex(E val) {
+                value = val;
             }
-            else {
-                v.parent.right = v.left;
-                v.left.parent = v.parent;
-            }
-            deleteFromList(v);
-            return true;
-        }
-        if (v.left == null) {
-            if (v.parent.left != null && v.parent.left.equals(v)) {
-                v.parent.left = v.right;
-                v.right.parent = v.parent;
-            }
-            else {
-                v.parent.right = v.right;
-                v.right.parent = v.parent;
-            }
-            deleteFromList(v);
-            return true;
-        }
-        Vertex next = v.next;
-        next.right.parent = next.parent;
-        if (next.parent.left.equals(next))
-            next.parent.left = next.right;
-        else
-            next.parent.right = next.right;
-        next.right = v.right;
-        next.left = v.left;
-        next.parent = v.parent;
-        if (next.parent.left.equals(v))
-            next.parent.left = next;
-        else
-            next.parent.right = next;
-        deleteFromList(v);
-        return true;
-    }
-
-    @Override
-    public Iterator<E> descendingIterator() {
-        return new DesSetIterator();
-    }
-
-    @Override
-    public MyTreeSet<E> descendingSet() {
-        return new descendingSet(this);
-    }
-
-    @Override
-    public E first() {
-        return iterator().next();
-    }
-
-    @Override
-    public E last() {
-        return last == null ? null : last.value;
-    }
-
-    private E descendLower(E e, Vertex v, E ans) {
-        if (v == null) return ans;
-        if (comp(v.value, e) < 0) {
-            return descendLower(e, v.right, comp(v.value, ans) < 0 ? ans : v.value);
-        }
-        if (comp(v.value, e) >= 0) {
-            return descendLower(e, v.left, ans);
-        }
-        return null;
-    }
-
-    @Override
-    public E lower(E e) {
-        return descendLower(e, root, null);
-    }
-
-    private E descendFloor(E e, Vertex v, E ans) {
-        if (v == null) return ans;
-        if (comp(v.value, e) <= 0) {
-            return descendFloor(e, v.right, comp(v.value, ans) <= 0 ? ans : v.value);
-        }
-        if (comp(v.value, e) > 0) {
-            return descendFloor(e, v.left, ans);
-        }
-        return null;
-    }
-
-    @Override
-    public E floor(E e) {
-        return descendFloor(e, root, null);
-    }
-
-    private E descendCeiling(E e, Vertex v, E ans) {
-        if (v == null) return ans;
-        if (comp(v.value, e) < 0) {
-            return descendCeiling(e, v.right, ans);
-        }
-        if (comp(v.value, e) >= 0) {
-            return descendCeiling(e, v.left, comp(v.value, ans) < 0 ? v.value : ans);
-        }
-        return null;
-    }
-
-    @Override
-    public E ceiling(E e) {
-        return descendCeiling(e, root, null);
-    }
-
-    private E descendHigher(E e, Vertex v, E ans) {
-        if (v == null) return ans;
-        if (comp(v.value, e) < 0) {
-            return descendHigher(e, v.right, ans);
-        }
-        if (comp(v.value, e) >= 0) {
-            return descendHigher(e, v.left, comp(v.value, ans) < 0 ? v.value : ans);
-        }
-        return null;
-    }
-
-    @Override
-    public E higher(E e) {
-        return descendHigher(e, root, null);
-    }
-
-    private class Vertex {
-        Vertex right;
-        Vertex left;
-        Vertex parent;
-        Vertex next;
-        Vertex prev;
-        E value;
-        Vertex(E val) {
-            value = val;
         }
     }
-
-    class descendingSet extends TreeSet<E> {
+    private class DescendingSet extends TreeSet<E>{
+        DescendingSet(TreeSet<E> revTree) {
+            this.tree = revTree.tree;
+        }
         @Override
         public E last() {
             return super.first();
@@ -301,60 +354,16 @@ public class TreeSet<E> extends AbstractSet<E> implements MyTreeSet<E> {
             return super.floor(e);
         }
 
-        private descendingSet(TreeSet<E> a) {
-            root = a.root;
-            size = a.size;
-            cmp = a.cmp;
-            last = a.last;
-            first = a.first;
-        }
-
+        @NotNull
         @Override
         public Iterator<E> iterator() {
             return super.descendingIterator();
         }
 
+        @NotNull
         @Override
         public Iterator<E> descendingIterator() {
             return super.iterator();
-        }
-    }
-
-    private class SetIterator implements Iterator<E> {
-        private Vertex current;
-
-        private SetIterator() {
-            current = first;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return current != last;
-        }
-
-        @Override
-        public E next() {
-            current = current.next;
-            return current.value;
-        }
-    }
-    private class DesSetIterator implements Iterator<E> {
-
-        private Vertex current;
-
-        private DesSetIterator() {
-            current = last;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return current != first;
-        }
-
-        @Override
-        public E next() {
-            current = current.prev;
-            return current.next.value;
         }
     }
 }
